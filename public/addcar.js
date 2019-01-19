8cabed251adb8fc6d149aa758972e41d4b03c0fb
Glitch (hello-sqlite) <none>
2019-01-19 22:26:06 +0000

    ⛳️📖 Checkpoint ./public/addcar.js:1326932/790

--- public/addcar.js
+++ public/addcar.js
@@ -8,18 +8,18 @@ console.log(submitButton)
 submitButton.onclick = () =>{
 console.log(addCarForm.elements)
 var formData =  {
-  id: 13,
   make: addCarForm[0].value,
   model: addCarForm[1].value,
   year: addCarForm[2].value,
   price: addCarForm[3].value,
   color: addCarForm[4].value,
-  description: addCarForm[0].value,
-  image: addCarForm[0].value,
-  specs: addCarForm[0].value,
-  highlights: addCarForm[0].value
+  description: addCarForm[5].value,
+  image: addCarForm[6].value,
+  specs: addCarForm[7].value,
+  highlights: addCarForm[8].value
 }
-
+// ||formData.model !== ''||formData.year !== ''||formData.price !== ''||formData.color !== ''  ||formData.description !== ''||formData.specs !== '' || formData.highlights !== ''
+if(formData.make !== ''){
 const xhr = new XMLHttpRequest;  
 const url = `/addnewcar?make=${formData.make}&model=${formData.model}&year=${formData.year}&price=${formData.price}
 &color=${formData.color}&description=${formData.description}&image=${formData.image}&specs=${formData.specs}&highlights=${formData.highlights}`;
@@ -33,4 +33,5 @@ console.log("Sent Request..")
   console.log("Data Sent", xhr.data)
   xhr.open('POST', url)
   xhr.send();
+  }else{ alert("Please Complete the Form before Submitting")}
 }
